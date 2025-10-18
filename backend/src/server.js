227f2c555f26da94cbd8d1dc45c5b9499f8b4300
athumaniMfaume <athumanimfaume1995@gmail.com>
2025-10-18 17:52:34 +0300

    prepare for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv'; 
 import rateLimiter from './middleware/rateLimiter.js';
+import path from 'path';
 
 
 dotenv.config();
@@ -12,27 +13,34 @@ const app = express();
 
 const PORT = process.env.PORT || 5001;
 
+const __dirname = path.resolve();
+
+
 
 // middleware to parse JSON request bodies
 app.use(express.json());
 app.use(rateLimiter);
-app.use(cors({
-  origin: 'http://localhost:5173', // frontend origin
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   allowedHeaders: ['Content-Type', 'Authorization']
+
+
+if (!process.env.NODE_ENV === 'production') {
+   app.use(
+    cors({
+      origin: 'http://localhost:5173',
 }));
+}
 
 
-// our simple logging and CORS middleware
-// app.use((req, res, next) => {
-//     console.log(`${req.method} request for ${req.url}`);
-//     res.setHeader('Access-Control-Allow-Origin', '*');  
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-//     next();
-// });
+
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));  
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+    });
+}
 
 connectDB().then(() => {
    app.listen(PORT, ()=>{
